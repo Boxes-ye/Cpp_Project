d27b2d63a465de8ebd6d2876ca8d1379e7fcb0f2
Boxes <liwuwukeke@gmail.com>
2025-02-08 21:59:39 +0800

    Revert "//圆类的定义函数"
    This reverts commit 72d16ed36f25a7ad87c2b7c3d4c6976388093604.

--- circle/circle.cpp
+++ circle/circle.cpp
@@ -1,25 +1,22 @@
 
-#include "circle.h"
+#include"circle.h"
+
 
 //设置半径
-void Circle::setr(double r)
-{
-    m_r = r;
+void Circle::setr(double r){
+    m_r=r;
 }
 //获取半径
-double Circle::getr()
-{
+double Circle::getr(){
     return m_r;
 }
 
 //设置圆心
-void Circle::setcenter(point center)
-{
+void Circle::setcenter(point center){
     m_center = center;
 }
 //获取圆心
-point Circle::getcenter()
-{
+point Circle::getcenter(){
     return m_center;
 }
 
