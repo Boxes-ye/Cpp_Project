5a84fb82e480bcb6a8a9935b1ad477033c3cf384
Boxes <liwuwukeke@gmail.com>
2025-02-09 11:01:50 +0800

    Update main.cpp

--- friend/main.cpp
+++ friend/main.cpp
@@ -1,8 +1,9 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-#include"building.h"
-int main(){
-    Goodgay gg;
+#include "building.h"
+int main()
+{
+    Goodgay gg; //首先创建Goodgay类 名字为gg
     gg.visit();
     gg.visit01();
 }
