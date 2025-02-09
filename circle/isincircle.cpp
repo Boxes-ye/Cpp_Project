4d02b1f692d0b42ebe1dd68a6d8fd17b321f2044
Boxes <liwuwukeke@gmail.com>
2025-02-09 10:30:09 +0800

    //判断点与圆位置关系

--- circle/isincircle.cpp
+++ circle/isincircle.cpp
@@ -1,17 +1,22 @@
-#include"isincircle.h"
-void isincircle(point p,Circle c){
+#include "isincircle.h"
+//判断点与圆位置关系
+void isincircle(point p, Circle c)
+{
 	//计算距离
-	double distance = sqrt((p.getX()-c.getcenter().getX())*(p.getX()-c.getcenter().getX())+(p.getY()-c.getcenter().getY())*(p.getY()-c.getcenter().getY()));
+	double distance = sqrt((p.getX() - c.getcenter().getX()) * (p.getX() - c.getcenter().getX()) + (p.getY() - c.getcenter().getY()) * (p.getY() - c.getcenter().getY()));
 	//计算半径的平方
-	double r = c.getr()*c.getr();
+	double r = c.getr() * c.getr();
 	//判断
-	if (distance > r){
-		cout<<"点在圆外"<<endl;
+	if (distance > r)
+	{
+		cout << "点在圆外" << endl;
 	}
-	else if (distance == r){
-		cout<<"点在圆上"<<endl;
+	else if (distance == r)
+	{
+		cout << "点在圆上" << endl;
 	}
-	else if (distance < r){
-		cout<<"点在圆内"<<endl;
+	else if (distance < r)
+	{
+		cout << "点在圆内" << endl;
 	}
 }
