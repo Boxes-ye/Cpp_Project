1c5d23fa0c1d304e7096eabec4b0cd2bac16bb06
Boxes <liwuwukeke@gmail.com>
2025-02-09 11:01:16 +0800

    //利用友元实现访问private类型变量

--- friend/building.cpp
+++ friend/building.cpp
@@ -1,16 +1,20 @@
-#include"building.h"
-Goodgay::Goodgay(){
+#include "building.h"
+Goodgay::Goodgay()
+{
     building = new Building;
 }
-void Goodgay::visit(){
-    cout<<" goodgay is looking "<<building->bedroom<<endl;
-    cout<<" goodgay is looking "<<building->sittingroom<<endl;
+void Goodgay::visit()
+{
+    cout << " goodgay is looking " << building->bedroom << endl;
+    cout << " goodgay is looking " << building->sittingroom << endl;
 }
-void Goodgay::visit01(){
+void Goodgay::visit01()
+{
     // cout<<"goodgay is looking"<<building->bedroom<<endl;
-    cout<<"goodgay01 is looking"<<building->sittingroom<<endl;
+    cout << "goodgay01 is looking" << building->sittingroom << endl;
 }
-Building::Building(){
-    this -> bedroom = "卧室" ;
-    this -> sittingroom = "客厅" ;
+Building::Building()
+{
+    this->bedroom = "卧室";
+    this->sittingroom = "客厅";
 }
