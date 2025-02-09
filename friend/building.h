b21f5253801e82252b699e6a2ac00764cc7e99a2
Boxes <liwuwukeke@gmail.com>
2025-02-09 11:01:44 +0800

    Building的函数声明

--- friend/building.h
+++ friend/building.h
@@ -1,27 +1,27 @@
-#include<iostream>
-#include<string.h>
+#include <iostream>
+#include <string.h>
 #ifndef BUILDING_H
 #define BUILDING_H
 using namespace std;
 class Building;
-class Goodgay{
-    private:
-    Building *building;
-    public:
-    Goodgay();
-    void visit();
+class Goodgay
+{
+private:
+    Building *building; // building是一个Building类的指针
+public:
+    Goodgay();    //构造函数
+    void visit(); //定义Goodgay类定义两个函数
     void visit01();
-    
 };
-class  Building{
-    friend void Goodgay::visit();
-    private:
-    string bedroom;
-    public:
+class Building
+{
+    friend void Goodgay::visit(); // Goodgay类中的visit作为Building的友元
+private:
+    string bedroom; //只有友元可以访问private变量
+public:
     string sittingroom;
-    public:
+
+public:
     Building();
-
-
 };
 #endif
